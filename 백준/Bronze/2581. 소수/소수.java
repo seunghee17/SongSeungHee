6a643e446b0f6f8882e843a4265ffaaf79d029ab
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-03-03 12:25:15 +0900

    [Bronze II] Title: 소수, Time: 180 ms, Memory: 16220 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+import java.time.*;
+ 
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int M = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int min = N;
+		int sum = 0;
+		
+		for(int i = M; i <= N; i++) {
+			boolean check = true;
+			if(i == 1) check = false;
+			
+			for(int j = 2; j < i; j++) {
+				if(i % j == 0) {
+					check = false;
+					break;
+				}
+			}
+			
+			if(check) {
+				if(min > i) min = i;
+				sum += i;
+			}
+		}
+		if(sum == 0) {
+			System.out.println(-1);
+		}else {
+			System.out.println(sum + "\n" + min);
+		}
+	}
+
+}
