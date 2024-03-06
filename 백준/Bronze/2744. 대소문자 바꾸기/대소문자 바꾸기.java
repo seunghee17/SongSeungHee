8ba727da7053da20912a40c33b45b3bf7aaa0082
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-03-06 19:54:08 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,27 +1,19 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 import java.math.*;
-import java.time.*;
- 
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
-		
-		char arr[] = new char[s.length()];
-		arr = s.toCharArray();
-		
-		for(int i =0; i<s.length(); i++) {
-			if(Character.isUpperCase(arr[i])){
-				arr[i] = Character.toLowerCase(arr[i]);
-			}else {
-				arr[i] = Character.toUpperCase(arr[i]);
-			}
-		}
-		
-		String reslut = String.valueOf(arr);
-		System.out.println(reslut);
-	}
-	
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        char arr[] = input.toCharArray();
+        for(int i=0; i<arr.length; i++){
+            if(Character.isUpperCase(arr[i])){
+                arr[i] = Character.toLowerCase(arr[i]);
+            }else{
+                arr[i] = Character.toUpperCase(arr[i]);
+            }
+        }
+        System.out.println(arr);
+    }
 }
