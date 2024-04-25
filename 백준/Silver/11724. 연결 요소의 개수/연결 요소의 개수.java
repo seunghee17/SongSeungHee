6541b4184e670ba8a0eb1813c7ea64d56c18d9c0
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-04-25 20:16:51 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 552 ms, Memory: 115880 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main{
+	static int[][] graph;
+	static boolean[] visited; 
+	static int cnt=0;
+	static int M,N;
+ 	public static void main(String[] args) throws IOException{
+ 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+ 		N = Integer.parseInt(st.nextToken());
+ 		M = Integer.parseInt(st.nextToken());
+ 		graph = new int[N+1][N+1];
+ 		visited = new boolean[N+1];
+ 		for(int i=0; i<M; i++) {
+ 			StringTokenizer sr = new StringTokenizer(br.readLine(), " ");
+ 			int x = Integer.parseInt(sr.nextToken());
+ 			int y = Integer.parseInt(sr.nextToken());
+ 			//인접 행렬을 만들어줌 
+ 			graph[x][y] = 1;
+ 			//무방향이므로 역방향을 만드는간선도 추가한다 
+ 			graph[y][x] = 1;
+ 		}
+ 		
+ 		for(int i=1; i<=N; i++) {
+ 			if(!visited[i]) {
+ 				dfs(i);
+ 				cnt++;
+ 			}
+ 		}
+ 		
+ 		System.out.println(cnt);
+	}	
+ 	
+ 	static void dfs(int node) {
+ 		visited[node] = true;
+ 		for(int i=1; i<=N; i++) {
+ 			if(graph[node][i] == 1 && !visited[i]) {
+					dfs(i);
+				}
+ 		}
+ 		
+ 	}
+ 
+ 	}
