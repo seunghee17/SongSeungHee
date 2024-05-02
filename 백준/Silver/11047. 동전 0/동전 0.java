07f95689cfdd39a2b7abf53d54a2a7425f7163b0
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-05-02 18:59:54 +0900

    [Silver IV] Title: 동전 0, Time: 132 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,27 +1,35 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.math.*;
 
-public class Main{
+public class Main {
+	 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
 	
- 	public static void main(String[] args) throws IOException{
- 		Scanner sc = new Scanner(System.in);
- 		int n = sc.nextInt();
- 		int k = sc.nextInt();
- 		int[] coin = new int[n];
- 		for(int i=0; i<n; i++) {
- 			coin[i] = sc.nextInt();
- 		}
- 		int count = 0;
- 		for(int i = n-1; i>=0; i--) {
- 			if(k >= coin[i]) {
- 				count += k/coin[i];
- 				k = k % coin[i];
+		int result = 0;
+		int nameun = K;
+		while(true) {
+			int max =0;
+			for(int i=N-1; i>=0; i--) {
+				if(arr[i] <= nameun) {
+					if(max < arr[i]) {
+						max = arr[i];
+					}
+				}
  			}
- 		}
- 		System.out.println(count);
-	}	
+			result += nameun/max;
+			nameun = nameun%max;
+			if(nameun==0) break;
+		}
+		System.out.println(result);
+	}
+ 
+}
  	
-	
-}	
