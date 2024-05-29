8052c74bce59cd54d5db283b348b5edc887c49bb
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-05-29 16:45:55 +0900

    [Silver II] Title: 부분수열의 합, Time: 132 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -5,37 +5,37 @@ import java.math.*;
 public class Main {
 	static int N;
 	static int S;
-	static int cnt=0;
 	static int[] arr;
+	static int cnt =0;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		S = Integer.parseInt(st.nextToken());
-		arr = new int[N];
+		
 		StringTokenizer sr = new StringTokenizer(br.readLine(), " ");
+		arr = new int[N];
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(sr.nextToken());
 		}
-		backTracking(0,0);
+		dfs(0,0);
 		if(S==0) {
-			System.out.println(cnt-1);
-		} else {
-			System.out.println(cnt);
-		}
+            System.out.println(cnt-1);
+        } else {
+            System.out.println(cnt);
+        }
 		
 	}
 	
-	static void backTracking(int depth, int sum) {
+	static void dfs(int depth, int sum) {
 		if(depth == N) {
 			if(sum == S) {
 				cnt++;
 			}
 			return;
 		}
-		backTracking(depth+1, sum+arr[depth]);
-		backTracking(depth+1, sum);
+		dfs(depth+1, sum+ arr[depth]);
+		dfs(depth+1, sum);
 	}
-	
- 
+
 }
