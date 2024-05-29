22cc0a05094be83032740c60659b651e055825c3
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-05-29 23:08:59 +0900

    [Bronze I] Title: 진법 변환 2, Time: 124 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -2,36 +2,28 @@ import java.io.*;
 import java.util.*;
 import java.math.*;
 
-public class Main{
-
+public class Main {
 	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int B = Integer.parseInt(st.nextToken());
-		
-		List<Integer> list = new ArrayList<Integer>();
-		int a=0;
+		StringBuilder sb = new StringBuilder();
 		while(true) {
-			list.add(N%B);
-			a = N/B;
+			int a = N%B;
 			N/=B;
-			
-			if(a==0) break;
-			
-		}
-		
-		Character[] arr = new Character[list.size()];
-		for(int i=list.size()-1; i>=0; i--) {
-			if(list.get(i) < 10) {
-				System.out.print(list.get(i));
-			}else {
-				arr[i] = (char)('A' + (list.get(i) - 10));
-				System.out.print(arr[i]);
+			if(a<10) {
+				sb.append(a);
+			} else {
+				sb.append((char)('A' + a-10));
 			}
-			
+			if(N==0) break;
 		}
+		sb.reverse();
+		System.out.println(sb);
 		
 	}
+
+ 
 }
