1a7c14db34dece5332fbd84b4d8779cf5f5d421a
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-05-30 18:01:19 +0900

    [Silver II] Title: 사탕 게임, Time: 184 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -2,81 +2,87 @@ import java.io.*;
 import java.util.*;
 import java.math.*;
 
-public class Main{
+public class Main {
+	static int N;
 	
-	public static void swapCandy(char[][] map, int r1, int c1, int r2, int c2) {
-		char temp = map[r1][c1];
-		map[r1][c1] = map[r2][c2];
-		map[r2][c2] = temp;
+	static void swapCandy(char[][] board, int row1, int row2, int col1, int col2) {
+		char temp = board[row2][col2];
+		board[row2][col2] = board[row1][col1];
+		board[row1][col1] = temp;
 	}
-	
-	public static int findmaxColumn(char[][] map) {
-		int N = map.length;
-		int max = 0;
-		for(int r = 0; r < N; r++) {
-			int len = 1;
-			
-			for(int c = 1; c < N; c++) {
-				if(map[r][c] == map[r][c-1]) {
+	static int findRow(char[][] board) {
+		
+		int max=0;
+		for(int i=0; i<N; i++) {
+			int len =1;
+			for(int j=0; j<N-1; j++) {
+				if(board[i][j] == board[i][j+1]) {
 					len++;
+					
+				} else {
 					max = Math.max(max, len);
-				}else {
+					len=1;
+					
+				}
+			}
+			max = Math.max(max, len);
+		}
+		return max;
+		
+	}
+	static int findCol(char[][] board) {
+		
+		int max=0;
+		for(int j=0; j<N; j++) {
+			int len=1;
+			for(int i=0; i<N-1; i++) {
+				if(board[i][j] == board[i+1][j]) {
+					len++;
+					
+				} else {
 					max = Math.max(max, len);
-					len = 1;
+					len=1;
+					
+				}
+			}
+			max = Math.max(max, len);
+		}
+		
+		return max;
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		char[][] board = new char[N][N];
+		
+		int max=0;
+		
+		//보드 완성 
+		for(int i=0; i<N; i++) {
+			String s = br.readLine();
+			for(int j=0; j<N; j++) {
+				board[i][j] = s.charAt(j);
+			}
+		}
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				if(j<N-1&&board[i][j] != board[i][j+1]) {
+					swapCandy(board,i,i, j, j+1);
+					max = Math.max(max, Math.max(findRow(board), findCol(board)));
+					swapCandy(board,i,i, j, j+1);
+				}
+				if(i<N-1 && board[i][j] != board[i+1][j]) {
+					swapCandy(board, i,i+1, j, j);
+					max = Math.max(max, Math.max(findRow(board), findCol(board)));
+					swapCandy(board, i,i+1, j, j);
 				}
 				
 			}
-			max = Math.max(max, len);
-			
-		}
-		return max;
+		
 	}
-	
-	public static int findmaxRow(char[][] map) {
-		int N = map.length;
-		int max = 0;
-		for(int c = 0; c < N; c++) {
-			int len = 1;
-			
-			for(int r = 1; r < N; r++) {
-				if(map[r][c] == map[r-1][c]) {
-					len++;
-					max = Math.max(max, len);
-				}else {
-					max = Math.max(max, len);
-					len = 1;
-				}
-				
-			}
-			max = Math.max(max, len);
-			
-		}
-		return max;
-	}
-	
-	public static void main(String[] args) throws IOException{
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int max = 0;
-		char[][] map = new char[N][N];
-		for(int i = 0; i<N; i++) {
-			map[i] = sc.next().toCharArray();
-		}
-		for(int i =0; i<N; i++) {
-			for(int j =0; j<N; j++) {
-				if( j+1 <N && map[i][j] != map[i][j+1]) {
-					swapCandy(map, i, j, i, j+1);
-					max = Math.max(max, Math.max(findmaxColumn(map), findmaxRow(map)));
-					swapCandy(map, i, j, i, j+1);
-				}
-				if(i+1<N && map[i][j] != map[i+1][j]) {
-					swapCandy(map, i, j, i+1, j);
-					max = Math.max(max, Math.max(findmaxColumn(map), findmaxRow(map)));
-					swapCandy(map, i, j, i+1, j);
-				}
-			}
-			
-		}
+		
 		System.out.println(max);
+	
 	}
+ 
 }
