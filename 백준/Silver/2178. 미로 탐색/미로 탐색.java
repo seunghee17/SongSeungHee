179c8c156816d2f835fd7af8bbdc1900507979ba
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-06-06 23:08:14 +0900

    [Silver I] Title: 미로 탐색, Time: 140 ms, Memory: 14712 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,55 +1,53 @@
 import java.io.*;
 import java.util.*;
+import java.math.*;
 
-public class Main {
-	static int N,M;
+public class Main { 
 	static int[][] graph;
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	static int[][] visit;
-	public static void main(String[] args) throws IOException{
+	static int[][] visited;
+	static int[] dx = {0,0,1,-1};
+	static int[] dy = {1,-1,0,0};
+	static int N;
+	static int M;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		graph = new int[N+1][M+1];
-		visit = new int[N+1][M+1];
-		//그래프 초기화 완료 
-		for(int i=1; i<=N; i++) {
+		visited = new int[N+1][M+1];
+		//graph완성 
+		for(int i=1; i<N+1; i++) {
 			char[] line = br.readLine().toCharArray();
-			for(int j=1; j<=M; j++) {
+ 			for(int j=1; j<M+1; j++) {
 				graph[i][j] = line[j-1] - '0';
-				
 			}
 		}
-		
 		bfs(1,1);
-		System.out.println(visit[N][M]);
+		System.out.println(visited[N][M]);
+		
 	}
 	
-	static void bfs(int x, int y) {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] {x,y}); //? 
-		visit[x][y] =1;
-		while(!q.isEmpty()) {
-			int[] now = q.poll();
-			int nx = now[0];
-			int ny = now[1];
+	static void bfs(int a, int b) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.offer(new int[] {a,b});
+		visited[a][b] = 1;
+		while(!queue.isEmpty()) {
+			int now[] = queue.poll();
 			for(int i=0; i<4; i++) {
-				int nextx = nx + dx[i];
-				int nexty = ny + dy[i];
-				if(nextx <1 || nextx>=N+1 || nexty<1 || nexty>=M+1) continue;
-				if(graph[nextx][nexty]==0) continue;
-				if(visit[nextx][nexty] ==0 && graph[nextx][nexty]==1) {
-					q.offer(new int[] {nextx, nexty});
-					visit[nextx][nexty] = visit[nx][ny] + 1;
+				int nextx = now[0] + dx[i];
+				int nexty = now[1] + dy[i];
+				if(nextx >=1 && nextx <= N && nexty >=1 && nexty <= M) {
+					if(graph[nextx][nexty] == 1 && visited[nextx][nexty] ==0) {
+						queue.offer(new int[] {nextx, nexty});
+						visited[nextx][nexty]= 1 + visited[now[0]][now[1]];
+					}
 				}
-				
 			}
-			
 		}
 	}
 	
 	
+	
+ 
 }
