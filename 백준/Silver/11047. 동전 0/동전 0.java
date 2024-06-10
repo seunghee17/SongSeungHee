938b96d6d0911e59fb5910ba87c2ade3ec5c34e4
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-06-10 15:33:14 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,35 +1,33 @@
 import java.io.*;
 import java.util.*;
+import java.math.*;
 
 public class Main {
-	 
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int[] arr = new int[N];
-		for(int i=0; i<N; i++) {
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
-	
-		int result = 0;
-		int nameun = K;
-		while(true) {
-			int max =0;
-			for(int i=0; i<N; i++) {
-				if(arr[i] <= nameun) {
-					if(max < arr[i]) {
+		int cnt = 0;
+		while(k!=0) {
+			int max = 0;
+			for(int i=n-1; i>=0; i--) {
+				if(arr[i] <= k) {
+					if(arr[i] > max) {
 						max = arr[i];
 					}
 				}
- 			}
-			result += nameun/max;
-			nameun = nameun%max;
-			if(nameun==0) break;
+			}
+			
+			cnt += k/max;
+			k = k%max;
 		}
-		System.out.println(result);
+		System.out.println(cnt);
 	}
- 
+
 }
- 	 
